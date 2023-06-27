20e5173b9a4f2a63682be36955b04ed2fae6dd6f
kicu <kicu.xxo@gmail.com>
2023-06-27 16:21:19 +0900

    curve test

--- lib/src/utils/painter/link_painter.dart
+++ lib/src/utils/painter/link_painter.dart
@@ -25,10 +25,10 @@ class LinkPainter extends CustomPainter {
     var radius = 10.0;
 
     path.moveTo(linkPoints[0].dx, linkPoints[0].dy);
-    // path.lineTo(linkPoints[1].dx, linkPoints[1].dy);
-    path.quadraticBezierTo(
-        linkPoints[1].dx, linkPoints[1].dy, linkPoints[2].dx, linkPoints[2].dy);
-    path.moveTo(linkPoints[2].dx, linkPoints[2].dy);
+    path.lineTo(linkPoints[1].dx, linkPoints[1].dy);
+    // path.quadraticBezierTo(
+    //     linkPoints[1].dx, linkPoints[1].dy, linkPoints[2].dx, linkPoints[2].dy);
+    // path.moveTo(linkPoints[2].dx, linkPoints[2].dy);
     // path.lineTo(linkPoints[2].dx, linkPoints[2].dy);
     path.quadraticBezierTo(
         linkPoints[2].dx, linkPoints[2].dy, linkPoints[3].dx, linkPoints[3].dy);
