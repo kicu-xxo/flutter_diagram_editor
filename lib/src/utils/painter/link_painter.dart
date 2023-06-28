2adcebc953181e7b36f23eb2324db870414c855f
kicu <kicu.xxo@gmail.com>
2023-06-28 17:09:15 +0900

    curve test

--- lib/src/utils/painter/link_painter.dart
+++ lib/src/utils/painter/link_painter.dart
@@ -43,16 +43,18 @@ class LinkPainter extends CustomPainter {
       );
     } else {
       path.moveTo(linkPoints[0].dx, linkPoints[0].dy);
+      path.conicTo(linkPoints[0].dx, linkPoints[0].dy, linkPoints[1].dx,
+          linkPoints[1].dy, 0.5);
 
-      for (int i = 0; i < linkPoints.length; i++) {
-        path.conicTo(linkPoints[i].dx, linkPoints[i].dy, linkPoints[i + 1].dx,
-            linkPoints[i + 1].dy, 0.5);
-        path.moveTo(linkPoints[i + 1].dx, linkPoints[i + 1].dy);
-      }
-
-      // for (int i = 2; i < linkPoints.length; i++) {
-      //   path.lineTo(linkPoints[i].dx, linkPoints[i].dy);
+      // for (int i = 0; i < linkPoints.length; i++) {
+      //   path.conicTo(linkPoints[i].dx, linkPoints[i].dy, linkPoints[i + 1].dx,
+      //       linkPoints[i + 1].dy, 0.5);
+      //   path.moveTo(linkPoints[i + 1].dx, linkPoints[i + 1].dy);
       // }
+
+      for (int i = 1; i < linkPoints.length; i++) {
+        path.lineTo(linkPoints[i].dx, linkPoints[i].dy);
+      }
       canvas.drawPath(path, paint);
     }
 // -------------- 반만 곡선--------------------
